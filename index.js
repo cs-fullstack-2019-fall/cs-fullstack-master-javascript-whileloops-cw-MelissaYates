817f5b5a88e25c4b7af2d88e5645ead6cce712c7
Melissa Yates <melissampeart@gmail.com>
2019-08-08 16:48:45 -0500

    while loops classwork assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+// // Classwork: JavaScript While/Do While Loops
+//
+// CW1
+//Print -10 through 5 using a counter and while loop.
+var counter = -10;
+while(counter<=5){
+    console.log(counter);
+    counter++;
+}
+//
+//     CW2
+//     Write a While loop that counts backwards from 10 to 0 Log the current count at the end of each iteration EXCEPT if the current count is 5
+// var x = 10;
+// while(x>= 0) {
+//  if(x>5 || x<5){
+//      console.log(x);
+//  }
+//     x--;
+// }
+
+// HINT: Use continue
+//
+// CW3
+// Asks the user Enter a word. Enter 'q' to quit. Create a while loop that will not quit until they enter 'q'.
+
+//
+// while(userEntry !=="q"){
+//     var userEntry = prompt("Please enter a word");
+//     if (userEntry =="abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ"){
+//         prompt("Please enter a word");
+//     }
+// }
+
